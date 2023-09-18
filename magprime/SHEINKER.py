108019faf253029751a53f3b317ed30bf6337da6
Alex Hoffmann <aphoff@umich.edu>
2023-09-18 16:10:44 -0400

    Working on Tutorial

--- magprime/SHEINKER.py
+++ magprime/SHEINKER.py
@@ -4,21 +4,34 @@ Date: 10/24/2022
 Description: Dual Magnetometer Interference Cancellation by Sheinker and Moldwin (2016)
 """
 import numpy as np
+from scipy.ndimage import uniform_filter1d
+
+detrend = True
+uf = 400 # Uniform Filter Size for detrending
 
 def clean(B, triaxial = True):
     """
     B: magnetic field measurements from the sensor array (n_sensors, axes, n_samples)
     triaxial: boolean for whether to use triaxial or uniaxial ICA
     """
+
+    if(detrend):
+        trend = uniform_filter1d(B, size=uf, axis = -1)
+        B -= trend
+
     if(triaxial):
         result = np.zeros((3, B.shape[-1]))
         for axis in range(3):
             result[axis] = cleanSheinker(B[:,axis,:])
-        return(result)
+
     else:
         "B: (n_sensors, n_samples)"
         result = cleanSheinker(B)
-        return(result)
+    
+    if(detrend):
+        result += np.mean(trend, axis=0)
+
+    return(result)
 
 def cleanSheinker(sig):
     d = sig[1]-sig[0]
