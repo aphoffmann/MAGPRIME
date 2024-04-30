be9983149f7f6c60ad6a27e9f0f08f178ad2a34f
Alex Hoffmann <aphoff@umich.edu>
2024-04-30 15:05:19 -0400

    Added .py to mssa file to make import work

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ DEPENDENCY_LINKS = [
 
 setup(
     name='magprime',
-    version='0.9935',
+    version='0.9936',
     description="Magnetic signal PRocessing, Interference Mitigation, and Enhancement (MAGPRIME)",
     author="Alex Paul Hoffmann",
     author_email='aphoff@umich.edu',
