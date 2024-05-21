e6849bc74bcb7c4fa25a7d001d59f5643455d6f2
Alex Hoffmann <aphoff@umich.edu>
2024-05-21 09:23:56 -0400

    Added utility to find coupling coefficients

--- magprime/utility/__init__.py
+++ magprime/utility/__init__.py
@@ -1,2 +1,3 @@
 from .data_loader import load_michibiki_data, load_swarm_data
 from .interpolation import mssa, linear, zero_fill
+import find_coupling_coefficients
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ DEPENDENCY_LINKS = [
 
 setup(
     name='magprime',
-    version='0.9957',
+    version='0.996',
     description="Magnetic signal PRocessing, Interference Mitigation, and Enhancement (MAGPRIME)",
     author="Alex Paul Hoffmann",
     author_email='aphoff@umich.edu',
