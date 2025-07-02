83c6943386c0787f0e69974b6dc3cf94c25416a7
Alex Hoffmann <alexpaulhoffmann@gmail.com>
2025-07-02 10:33:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ DEPENDENCY_LINKS = [
 
 setup(
     name='magprime',
-    version='1.8.0.4',
+    version='1.8.0.5',
     description="Magnetic signal PRocessing, Interference Mitigation, and Enhancement (MAGPRIME)",
     author="Alex Paul Hoffmann",
     author_email='aphoff@umich.edu',
